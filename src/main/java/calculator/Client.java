645dc8a44a7f8377bb5a1bc667beba5f1ba1b247
kimhyunso <gustn9515@gmail.com>
2024-05-17 10:22:00 +0900

    feat(Client): 기능 테스트 구현

--- src/main/java/calculator/Client.java
+++ src/main/java/calculator/Client.java
@@ -1,9 +1,5 @@
 package calculator;
 
-
-import java.util.Arrays;
-import java.util.Scanner;
-
 public class Client {
     public static void main(String[] args) {
         String line = "//;\n1;2;3;4;5";
