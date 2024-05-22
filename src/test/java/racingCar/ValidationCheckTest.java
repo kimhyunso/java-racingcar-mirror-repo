2a85936a9d0fe2899bc9035e9dc5d6ac2c9e4f5f
kimhyunso <gustn9515@gmail.com>
2024-05-22 23:27:52 +0900

    fix(ValidationCheckTest): ValidationCheckTest 자동차 이름길이 테스트 케이스 수정

--- src/test/java/racingCar/ValidationCheckTest.java
+++ src/test/java/racingCar/ValidationCheckTest.java
@@ -1,7 +1,9 @@
 package racingCar;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import racingCar.domain.CarName;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -10,9 +12,10 @@ public class ValidationCheckTest {
     @DisplayName("자동차이름이 1글자 이상 5글자 미만 기능")
     @Test
     void 자동차이름_1이상_5미만(){
-        assertThat(ValidationCheck.isGreaterThan("gabie")).isTrue();
-        assertThat(ValidationCheck.isGreaterThan("abc")).isFalse();
-        assertThat(ValidationCheck.isGreaterThan("")).isTrue();
+        Assertions.assertThatThrownBy(()->{
+            assertThat(new CarName(""));
+            assertThat(new CarName("qwert"));
+        }).isInstanceOf(IllegalArgumentException.class);
     }
 
 }
